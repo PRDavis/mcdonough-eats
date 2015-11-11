9ea1f2fcad2168e213239a652805a00e45b561d0
Paul Davis <paulandcindy@gmail.com>
2015-11-11 00:31:47 -0500

    added libraries and initial model array

---
+++ dev/js/mcdonough-eats.js
@@ -0,0 +1,70 @@
+/*  The McDonoughEats function acts as the ViewModel for this application.
+It also holds the model but maintains seperation of concerns using the MVVM design patter.
+*/
+
+var McDonoughEats = function ()
+{
+
+/* initialPlaces is an array of local restaurant names and phone numbers. */
+var initialPlaces =
+[
+  {
+    name: "Kirby G's",
+    tel: '6785838777',
+    visible:'TRUE'
+  },
+  {
+    name: 'Queen Bee Coffee',
+    tel: '6788832233',
+    visible:'TRUE'
+  },
+  {
+    name: 'Deep South Deli',
+    tel: '6783008999',
+    visible:'TRUE'
+  },
+  {
+    name: 'Pasta Max',
+    tel: '7703209311',
+    visible:'TRUE'
+  },
+  {
+    name: 'South Side Diner',
+    tel: '6787592690',
+    visible:'TRUE'
+  },
+  {
+    name: 'Gritz',
+    tel: '7709140448',
+    visible:'TRUE'
+  },
+  {
+    name: 'Seasons Bistro',
+    tel: '6788144995',
+    visible:'TRUE'
+  },
+  {
+    name: "Truett's Grill",
+    tel: '6784322221',
+    visible:'TRUE'
+  },
+  {
+    name: "Mikie's Big Burger",
+    tel: '7709544306',
+    visible:'TRUE'
+  }
+];
+
+
+
+
+
+
+
+
+  $(init);
+
+  return{
+
+  };
+}();
