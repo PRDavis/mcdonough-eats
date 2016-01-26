54c45f56093375894aa3777b0837a509831054c2
Paul Davis <paulandcindy@gmail.com>
2016-01-25 23:33:12 -0500

    added gulp task runner to manage output files

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,63 @@
 // include gulp and plugins
 var
   gulp = require('gulp'),
-  del = require('del');
+  del = require('del'),
+  pleeease = require('gulp-pleeease'),
+  htmlclean = require('gulp-htmlclean'),
+  newer = require('gulp-newer'),
+  pkg = require('./package.json'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  size = require('gulp-size');
+
+
+//environment variable check
+// setting Node environment variable to either development or production to control the output files
+// development are readable and production files are minified and otherwise processed
+var
+devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production'),
+
+
 
 // file locations
-var
+
   source = 'dev/',
   dest = 'dist/';
+  console.log('devBuild = ', devBuild);
 
+html = {
+  in: source + '*.html',
+  watch: [source + '*.html'],
+  out: dest,
+    context: {
+      devBuild: devBuild
+  }
+};
+
+
+css = {
+  in: source + 'css/**/*',
+  watch: [source + 'css/**/*'],
+  out: dest +'css/',
+  pleeeaseOpts: {
+    "autoprefixer": {browsers: ['last 2 versions', '> 2%'] },
+    "rem": ['16px'],
+    "pseudoElements": true,
+    "mqpacker": true,
+    "minifier": !devBuild
+  }
+  };
+
+js = {
+  in: source + 'js/**/*',
+  watch: [source + 'js/**/*'],
+  out: dest +'js/',
+  filename: 'main.js'
+  };
+
+
+//show build type
+console.log(pkg.name + ' ' + pkg.version + ', ' + (devBuild ? 'development' : 'production')+' build');
 // clean the build folder
 gulp.task('clean',function() {
   del([
@@ -17,7 +67,52 @@ gulp.task('clean',function() {
   ]);
 });
 
-// default task
-gulp.task('default', function() {
+//build HTML
+gulp.task('html', function() {
+  var page = gulp.src(html.in);
+  if(!devBuild){
+    page = page
+    .pipe(size({title: 'HTML in'}))
+    .pipe(htmlclean())
+    .pipe(size({title: 'HTML out'}));
+  }
+  return page.pipe(gulp.dest(html.out));
+});
+// build css
 
+gulp.task('css', function() {
+  return gulp.src(css.in)
+  .pipe(size({title: 'CSS in'}))
+  .pipe(pleeease(css.pleeeaseOpts))
+  .pipe(size({title: 'CSS out'}))
+  .pipe(gulp.dest(css.out));
+});
+
+// build js
+
+gulp.task('js', function() {
+  if(devBuild){
+  return gulp.src(js.in)
+  .pipe(newer(js.out))
+  .pipe(gulp.dest(js.out));
+}
+  else {
+    del([
+      dest + 'js/*'
+    ]);
+    return gulp.src(js.in)
+    .pipe(size({title: 'JS in'}))
+    .pipe(stripdebug())
+    .pipe(uglify())
+    .pipe(size({title: 'JS out'}))
+    .pipe(gulp.dest(js.out));
+  }
+});
+
+// default task
+gulp.task('default', ['html', 'css', 'js'], function() {
+// html changes
+gulp.watch(html.watch, ['html']);
+gulp.watch(css.watch, ['css']);
+gulp.watch(js.in, ['js']);
 });
